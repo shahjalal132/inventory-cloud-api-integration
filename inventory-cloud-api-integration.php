8fa73bb9ef30cdd643027b7e8a0edbbba06f1c6d
Shah Jalal <rjshahjalal132@gmail.com>
2025-09-30 16:06:21 +0600

    update plugin version to 2.4.0

--- inventory-cloud-api-integration.php
+++ inventory-cloud-api-integration.php
@@ -7,7 +7,7 @@
  * Author:      Shah Jalal
  * Author URI:  https://github.com/shahjalal132
  * Description: Update inventory in woocommerce and store bidirectionally
- * Version:     2.3.0
+ * Version:     2.4.0
  * text-domain: inventory-cloud
  * Domain Path: /languages
  * 
